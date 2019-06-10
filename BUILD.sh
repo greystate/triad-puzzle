745c1b08c27d5719b67d9bd683a5b922c2d95141
Chriztian Steinmeier <chriztian@steinmeier.dk>
2019-06-10 09:43:58 +0200

    Update BUILD

--- BUILD.sh
+++ BUILD.sh
@@ -3,7 +3,7 @@ PROJECT_ROOT="$TM_PROJECT_DIRECTORY"
 BUILT_ASSETS="$PROJECT_ROOT/build/assets"
 WEB_ASSETS="$PROJECT_ROOT/assets"
 
-rm "$PROJECT_ROOT/config.codekit3"
+# rm "$PROJECT_ROOT/config.codekit3"
 
 cp "$BUILT_ASSETS/*.*" "$WEB_ASSETS"
 cp "$PROJECT_ROOT/build/triad-puzzle.html" "$PROJECT_ROOT/index.html"
