fa524694f7d5ded5e79927fda2087bebace77009
Chriztian Steinmeier <chriztian@steinmeier.dk>
2019-06-10 09:36:08 +0200

    Add BUILD file

---
+++ BUILD.sh
@@ -0,0 +1,11 @@
+PROJECT_ROOT="$TM_PROJECT_DIRECTORY"
+
+BUILT_ASSETS="$PROJECT_ROOT/build/assets"
+WEB_ASSETS="$PROJECT_ROOT/assets"
+
+rm "$PROJECT_ROOT/config.codekit3"
+
+cp "$BUILT_ASSETS/*.*" "$WEB_ASSETS"
+cp "$PROJECT_ROOT/build/triad-puzzle.html" "$PROJECT_ROOT/index.html"
+
+rm -rf "$PROJECT_ROOT/build"
