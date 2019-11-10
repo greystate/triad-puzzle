f9266be35c5f52200d92f86023d6d6b2865271d7
Chriztian Steinmeier <chriztian@steinmeier.dk>
2019-11-10 14:48:15 +0100

    New release

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,18 @@
-'use strict';
+"use strict";
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 (function () {
   var ACCIDENTAL, ACCIDENTALS, DEFAULTS, PIECES, ROOTS, SPACE_KEY, TYPE, TYPES, Triad, TriadPuzzle, TriadPuzzleController, randomInt;
@@ -12,7 +20,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   randomInt = function randomInt() {
     var min = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
     var max = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
@@ -21,28 +28,32 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     natural: 0,
     sharp: 1
   };
-
   TYPE = {
     major: 0,
     minor: 1,
     diminished: 2,
-    augmented: 3
+    augmented: 3,
+    add9: 4,
+    madd9: 5,
+    sus2: 6,
+    sus4: 7
   };
-
   DEFAULTS = {
-    pitch: 0, // 0-11
-    acc: 0, // -1, 0, 1    (b, [nothing/natural], #)
-    type: 0 // 0-3         (major, minor, diminished, augmented)
+    pitch: 0,
+    // 0-11
+    acc: 0,
+    // -1, 0, 1    (b, [nothing/natural], #)
+    type: 0 // 0-7         (major, minor, diminished, augmented etc.)
+
   };
+  ROOTS = 'C D EF G A B'; //              012345678901
 
-  ROOTS = 'C D EF G A B';
-
-  //              012345678901
   ACCIDENTALS = 'b #';
+  TYPES = ['', 'm', 'dim', '+', 'add9', 'm(add9)', 'sus2', 'sus4'];
 
-  TYPES = ['', 'm', 'dim', '+'];
-
-  Triad = function () {
+  Triad =
+  /*#__PURE__*/
+  function () {
     function Triad() {
       var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
@@ -58,18 +69,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         if (this.pitch == null) {
           this.pitch = DEFAULTS.pitch;
         }
+
         if (this.acc == null) {
           this.acc = DEFAULTS.acc;
         }
+
         if (this.type == null) {
           this.type = DEFAULTS.type;
         }
       }
+
       this.validate();
     }
 
     _createClass(Triad, [{
-      key: 'validate',
+      key: "validate",
       value: function validate() {
         switch (this.acc) {
           case -1:
@@ -82,7 +96,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 // Can't be represented as a "flat"
                 return this.acc = 0;
             }
+
             break;
+
           case 0:
             switch (this.pitch) {
               case 1:
@@ -90,10 +106,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
               case 6:
               case 8:
                 return this.acc = 1;
+
               case 10:
                 return this.acc = -1;
             }
+
             break;
+
           case 1:
             switch (this.pitch) {
               case 2:
@@ -104,28 +123,32 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
                 // Can't be represented as a "sharp"
                 return this.acc = 0;
             }
+
         }
       }
     }, {
-      key: 'rootName',
+      key: "rootName",
       value: function rootName() {
-        return '' + this.baseName() + this.accidentalSign();
+        return "".concat(this.baseName()).concat(this.accidentalSign());
       }
     }, {
-      key: 'baseName',
+      key: "baseName",
       value: function baseName() {
         var index;
         index = this.pitch - this.acc;
+
         if (index > 11) {
           index = 0;
         }
+
         if (index < 0) {
           index = 11;
         }
+
         return ROOTS.charAt(index);
       }
     }, {
-      key: 'accidentalSign',
+      key: "accidentalSign",
       value: function accidentalSign() {
         var forHTML = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
@@ -133,46 +156,49 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           switch (this.acc) {
             case -1:
               return 'b';
+
             case 1:
               return '#';
+
             default:
               return '';
           }
         }
       }
     }, {
-      key: 'typeName',
+      key: "typeName",
       value: function typeName() {
         return TYPES[this.type];
       }
     }, {
-      key: 'toSymbol',
+      key: "toSymbol",
       value: function toSymbol() {
-        return '' + this.rootName() + this.typeName();
+        return "".concat(this.rootName()).concat(this.typeName());
       }
     }, {
-      key: 'toHTML',
+      key: "toHTML",
       value: function toHTML() {
         var acc, accEntity, accHTML, base, baseHTML, parts, type, typeHTML;
         base = this.baseName();
         acc = this.accidentalSign();
         accEntity = this.accidentalSign(true);
         type = this.typeName();
-        baseHTML = '<data class="root" value="' + base + '">' + base + '</data>';
-        accHTML = acc !== '' ? '<data class="acc" value="' + acc + '">' + acc + '</data>' : '';
-        typeHTML = type !== '' ? '<data class="triad" value="' + type + '">' + type + '</data>' : '';
+        baseHTML = "<data class=\"root\" value=\"".concat(base, "\">").concat(base, "</data>");
+        accHTML = acc !== '' ? "<data class=\"acc\" value=\"".concat(acc, "\">").concat(acc, "</data>") : '';
+        typeHTML = type !== '' ? "<data class=\"triad\" value=\"".concat(type, "\">").concat(type, "</data>") : '';
         parts = [baseHTML, accHTML, typeHTML].filter(function (val) {
           return val !== '';
         });
-        return '<span class="piece">\n\t' + parts.join('\n\t') + '\n</span>';
+        return "<span class=\"piece\">\n\t".concat(parts.join('\n\t'), "\n</span>");
       }
     }], [{
-      key: 'fromSymbol',
+      key: "fromSymbol",
       value: function fromSymbol(symbol) {
         var acc, accidental, match, pitch, root, symbolRE, triadtype, type;
         symbolRE = /^([CDEFGAB])([b\#]?)(\+|m|dim)?$/; // root
         // optional accidental
         // optional triad type
+
         if (symbolRE.test(symbol)) {
           var _symbol$match = symbol.match(symbolRE);
 
@@ -182,11 +208,14 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           root = _symbol$match2[1];
           accidental = _symbol$match2[2];
           triadtype = _symbol$match2[3];
-
           acc = accidental !== '' ? ACCIDENTALS.indexOf(accidental) - 1 : 0;
           pitch = ROOTS.indexOf(root) + acc;
           type = triadtype !== void 0 ? TYPES.indexOf(triadtype) : 0;
-          return new Triad({ pitch: pitch, acc: acc, type: type });
+          return new Triad({
+            pitch: pitch,
+            acc: acc,
+            type: type
+          });
         }
       }
     }]);
@@ -195,20 +224,39 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   }();
 
   window.Triad = Triad;
-
   SPACE_KEY = 32;
-
   PIECES = 8;
 
-  TriadPuzzle = function () {
+  TriadPuzzle =
+  /*#__PURE__*/
+  function () {
     function TriadPuzzle() {
       var _this = this;
 
-      var sheetSelector = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '.sheet';
+      var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
       _classCallCheck(this, TriadPuzzle);
 
-      this.sheetSelector = sheetSelector;
+      this.sheetSelector = options.sheetSelector;
+      this.majorAndMinorOnly = options.majorAndMinorOnly;
+      this.add9s = options.add9s;
+
+      if (this.sheetSelector == null) {
+        this.sheetSelector = '.sheet';
+      }
+
+      if (this.majorAndMinorOnly == null) {
+        this.majorAndMinorOnly = false;
+      }
+
+      if (this.add9s == null) {
+        this.add9s = false;
+      }
+
+      if (this.suss == null) {
+        this.suss = false;
+      }
+
       this.setupSheet();
       document.body.addEventListener('keypress', function (event) {
         if (event.keyCode === SPACE_KEY) {
@@ -219,6 +267,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       document.body.addEventListener('touchend', function (event) {
         var target;
         target = event.target;
+
         if (target.closest('.piece')) {
           return _this.setupSheet();
         }
@@ -226,21 +275,20 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
 
     _createClass(TriadPuzzle, [{
-      key: 'setupSheet',
+      key: "setupSheet",
       value: function setupSheet() {
         var keepExisting = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
-
         var count, hanger, item, previousTriad, rotation, sheet, triad;
         sheet = document.querySelector(this.sheetSelector);
         hanger = document.createDocumentFragment();
         previousTriad = null;
-        count = 1;
-
-        // We need 8 pieces, but we don't want to have
+        count = 1; // We need 8 pieces, but we don't want to have
         // two identical triads follow each other
+
         while (count <= PIECES) {
           item = document.createElement('li');
           triad = this.getRandomTriad();
+
           if (triad.toSymbol() !== previousTriad) {
             rotation = randomInt(-2, 3);
             item.innerHTML = triad.toHTML();
@@ -250,38 +298,57 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
             count++;
           }
         }
+
         if (keepExisting !== true) {
           sheet.innerHTML = '';
         }
+
         return sheet.appendChild(hanger);
       }
     }, {
-      key: 'getRandomTriad',
+      key: "getRandomTriad",
       value: function getRandomTriad() {
         var acc, pitch, type;
         pitch = randomInt(0, 11);
         acc = randomInt(-1, 1);
-        type = randomInt(0, 1);
-        return new Triad({ pitch: pitch, acc: acc, type: type });
+        type = randomInt(0, this.majorAndMinorOnly ? 1 : 3);
+
+        if (this.add9s && type <= 1) {
+          if (randomInt(0, 20) > 16) {
+            type += 4;
+          }
+        }
+
+        if (this.suss && type <= 1) {
+          if (randomInt(0, 20) > 13) {
+            type += 6;
+          }
+        }
+
+        return new Triad({
+          pitch: pitch,
+          acc: acc,
+          type: type
+        });
       }
     }]);
 
     return TriadPuzzle;
-  }();
+  }(); // Export
 
-  // Export
-  window.TriadPuzzle = TriadPuzzle;
 
-  // @codekit-prepend "helpers.coffee"
+  window.TriadPuzzle = TriadPuzzle; // @codekit-prepend "helpers.coffee"
   // @codekit-prepend "triad.coffee"
-
   // Global app object 
+
   if (window.app == null) {
     window.app = {};
-  }
+  } // Main controller for the page's functions
 
-  // Main controller for the page's functions
-  TriadPuzzleController = function () {
+
+  TriadPuzzleController =
+  /*#__PURE__*/
+  function () {
     function TriadPuzzleController() {
       _classCallCheck(this, TriadPuzzleController);
 
@@ -289,19 +356,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
 
     _createClass(TriadPuzzleController, [{
-      key: 'setup',
+      key: "setup",
       value: function setup() {
-        return this.puzzle = new TriadPuzzle();
+        return this.puzzle = new TriadPuzzle({
+          majorAndMinorOnly: true
+        });
       }
     }]);
 
     return TriadPuzzleController;
-  }();
+  }(); // Start everything when the page is ready
 
-  // Start everything when the page is ready
+
   document.addEventListener('DOMContentLoaded', function (event) {
     return app.controller = new TriadPuzzleController();
-  });
+  }); // @codekit-prepend "triad-puzzle.coffee"
+}).call(void 0);
 
-  // @codekit-prepend "triad-puzzle.coffee"
-}).call(undefined);
+//# sourceMappingURL=app.js.map
