9539e49d5da2bd944fd72d1be2e10f61a6b34c7b
Chriztian Steinmeier <chriztian@steinmeier.dk>
2019-11-19 00:50:09 +0100

    Update BUILD.sh
    Wildcards are apparently treated verbatim when inside (double)quotes ? TIL

--- BUILD.sh
+++ BUILD.sh
@@ -5,7 +5,7 @@ WEB_ASSETS="$PROJECT_ROOT/assets"
 
 # rm "$PROJECT_ROOT/config.codekit3"
 
-cp "$BUILT_ASSETS/*.*" "$WEB_ASSETS"
+cp $BUILT_ASSETS/*.* $WEB_ASSETS
 cp "$PROJECT_ROOT/build/triad-puzzle.html" "$PROJECT_ROOT/index.html"
 
-rm -rf "$PROJECT_ROOT/build"
+# rm -rf "$PROJECT_ROOT/build"
