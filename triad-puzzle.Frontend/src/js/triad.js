151d3d977e2247ba3094bf51370b22dee39f9f14
Chriztian Steinmeier <chriztian@steinmeier.dk>
2023-09-14 23:12:04 +0200

    Spec the static `.fromSymbol()` method and implement

--- triad-puzzle.Frontend/src/js/triad.js
+++ triad-puzzle.Frontend/src/js/triad.js
@@ -1,12 +1,65 @@
+const ACCIDENTAL = {
+	flat:      -1,
+	natural:    0,
+	sharp:      1
+}
+
+const TYPE = {
+	major:      0,
+	minor:      1,
+	diminished: 2,
+	augmented:  3,
+	add9:       4,
+	madd9:      5,
+	sus2:       6,
+	sus4:       7
+}
+
+const DEFAULTS = {
+	pitch:      0,   // 0-11
+	acc:        0,   // -1, 0, 1  (b, [nothing/natural], #)
+	type:       0    // 0-7       (major, minor, diminished, augmented etc.)
+}
+
+const ROOTS = 'C D EF G A B'
+//             012345678901
+
+const ACCIDENTALS = 'b #'
+const TYPES       = ['', 'm', 'dim', '+', 'add9', 'm(add9)', 'sus2', 'sus4']
+const ARIA_TYPES  = ['major', 'minor', 'diminished', 'augmented', 'add nine', 'minor add nine', 'sus two', 'sus four']
+
 class Triad {
-	constructor(options = { pitch: 0, acc: 0, type: 0}) {
+	constructor(options = { }) {
 		if (typeof(options) === 'string') {
-			// nop
+			return Triad.fromSymbol(options)
 		} else {
-			this.pitch = options.pitch
-			this.acc = options.acc
-			this.type = options.type
+			({ pitch: this.pitch, acc: this.acc, type: this.type } = options)
+			if (this.pitch == null) { this.pitch = DEFAULTS.pitch }
+			if (this.acc == null) { this.acc = DEFAULTS.acc }
+			if (this.type == null) { this.type = DEFAULTS.type }
 		}
+	}
+
+	static fromSymbol(symbol) {
+		let result
+
+		// ^
+		// ([CDEFGAB])     # root
+		// ([b\#]?)        # optional accidental
+		// (\+|m|dim)?     # optional triad type
+		// $
+		const symbolRE = /^([CDEFGAB])([b\#]?)(\+|m|dim)?$/
+		let match, root, accidental, triadtype
+
+		if (symbolRE.test(symbol)) {
+			[match, root, accidental, triadtype] = symbol.match(symbolRE)
+			const acc = accidental !== '' ? ACCIDENTALS.indexOf(accidental) - 1 : 0
+			const pitch = ROOTS.indexOf(root) + acc
+			const type = triadtype !== undefined ? TYPES.indexOf(triadtype) : 0
+			result = new Triad({ pitch, acc, type })
+		}
+
+		return result
 	}
 }
 
--- triad-puzzle.Frontend/test/spec/TriadSpec.js
+++ triad-puzzle.Frontend/test/spec/TriadSpec.js
@@ -11,7 +11,7 @@ describe('Triad', function() {
 				type: 1
 			})
 
-			//this.notherTriad = new Triad('Abdim')
+			this.notherTriad = new Triad('Abdim')
 		})
 
 		it('defaults to a C major triad', function() {
@@ -25,5 +25,53 @@ describe('Triad', function() {
 			expect(this.otherTriad.acc).toEqual(1)
 			expect(this.otherTriad.type).toEqual(1)
 		})
+
+		it('can create with a symbol too', function() {
+			expect(this.notherTriad.pitch).toEqual( 8)
+			expect(this.notherTriad.acc).toEqual(-1)
+			expect(this.notherTriad.type).toEqual( 2)
+		})
 	})
+
+	describe('.fromSymbol()', function() {
+		it('builds a minor Triad from its symbol', function() {
+			const triad = Triad.fromSymbol('C#m')
+
+			expect(triad.pitch).toEqual(1)
+			expect(triad.acc).toEqual(1)
+			expect(triad.type).toEqual(1)
+		})
+
+		it('builds an augmented Triad from its symbol', function() {
+			const triad = Triad.fromSymbol('Ab+')
+
+			expect(triad.pitch).toEqual( 8)
+			expect(triad.acc).toEqual(-1)
+			expect(triad.type).toEqual( 3)
+		})
+
+		it('builds a major Triad from its symbol', function() {
+			const triad = Triad.fromSymbol('F')
+
+			expect(triad.pitch).toEqual(5)
+			expect(triad.acc).toEqual(0)
+			expect(triad.type).toEqual(0)
+		})
+
+		it('builds a diminished Triad from its symbol', function() {
+			const triad = Triad.fromSymbol('Bbdim')
+
+			expect(triad.pitch).toEqual(10)
+			expect(triad.acc).toEqual(-1)
+			expect(triad.type).toEqual(2)
+		})
+
+		it('Shouldn\'t build a Triad from a wrong symbol', function() {
+			const triad = Triad.fromSymbol('Pdiddy')
+
+			expect(triad).toBeUndefined()
+		})
+	})
+
+
 })
