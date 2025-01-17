ced5cd47703cbf8e69b11fc24f0bbd630dfc3dbb
Chriztian Steinmeier <greystate@users.noreply.github.com>
2025-01-17 17:04:36 +0100

    Start specs for TriadPuzzle

--- triad-puzzle.Frontend/test/spec/TriadPuzzleSpec.js
+++ triad-puzzle.Frontend/test/spec/TriadPuzzleSpec.js
@@ -1,9 +1,23 @@
-import TriadPuzzle from '../../src/js/triad-puzzle.js'
+import TriadPuzzle from '../../src/js/modules/triad-puzzle.js'
 
 describe('TriadPuzzle', function() {
+
 	describe('creation', function() {
-		it('Says hello', function() {
-			expect(2).toBe(3)
+		const puzzle = new TriadPuzzle()
+
+		it('can create a random triad', function() {
+			this.randomTriad = puzzle.getRandomTriad()
+
+			expect(this.randomTriad.pitch).toBeGreaterThanOrEqual(0)
+			expect(this.randomTriad.pitch).toBeLessThanOrEqual(11)
+
+			expect(this.randomTriad.acc).toBeGreaterThanOrEqual(-1)
+			expect(this.randomTriad.acc).toBeLessThanOrEqual(1)
+
+			expect(this.randomTriad.type).toBeGreaterThanOrEqual(0)
+			expect(this.randomTriad.type).toBeLessThanOrEqual(7)
 		})
 	})
+
+	it('')
 })
