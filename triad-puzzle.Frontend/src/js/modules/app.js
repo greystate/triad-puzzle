6d94cfd2048c76c5cd66a57634e00233218c5764
Chriztian Steinmeier <greystate@users.noreply.github.com>
2025-01-19 14:07:12 +0100

    Handle responsiveness

--- triad-puzzle.Frontend/src/js/modules/app.js
+++ triad-puzzle.Frontend/src/js/modules/app.js
@@ -1,13 +1,57 @@
-//
-//	This is the app's main JS file.
-//	
-//	It must export a single `default function` that gets imported and
-//	executed by the two wrapper files in the parent `js` folder.
-//
-
-import Point from './point.js'
+import TriadPuzzle from './triad-puzzle.js'
+import '../../../vendor/theme-picker/theme-picker.js'
+import '../../../vendor/wakelock-toggle/wakelock-toggle.js'
 
 export default function() {
-	const pointTest = new Point(5, 10)
-	console.log(pointTest.draw())
+	const settings = getSettings()
+	applySettings(settings)
+
+	const puzzle = new TriadPuzzle(settings)
+
+	document.querySelector('.settings').addEventListener('change', (event) => {
+		const target = event.target
+		if (target.nodeName != 'INPUT') { return }
+		const newValue = target.checked
+		const setting = target.id
+		puzzle[setting] = newValue
+
+		saveSetting(setting, newValue)
+	})
+}
+
+function getSettings() {
+	const store = getStorage()
+	const settings = { }
+
+	settings.add9s = store.getItem('add9s') === 'true'
+	settings.majorAndMinorOnly = store.getItem('majorAndMinorOnly') === 'true'
+
+	return settings
+}
+
+function saveSetting(setting, value) {
+	const store = getStorage()
+
+	store.setItem(setting, value)
+}
+
+function applySettings(settings) {
+	const majorAndMinorToggle = document.querySelector('#majorAndMinorOnly')
+	const add9sToggle = document.querySelector('#add9s')
+	if (majorAndMinorToggle) { majorAndMinorToggle.checked = settings.majorAndMinorOnly }
+	if (add9sToggle) { add9sToggle.checked = settings.add9s }
+}
+
+function getStorage() {
+	return localStorage || getDummyStorage()
+}
+
+function getDummyStorage() {
+	let emptyFunction = new Function()
+
+	return {
+		getItem: emptyFunction,
+		setItem: emptyFunction,
+		removeItem: emptyFunction
+	}
 }
