e4aeb99178addf225d52c4ed584f47cbfcc79beb
Chriztian Steinmeier <greystate@users.noreply.github.com>
2025-01-21 00:24:51 +0100

    Add DEPLOY task

---
+++ DEPLOY.sh
@@ -0,0 +1,8 @@
+PROJECT_DIR=$TM_PROJECT_DIRECTORY
+BUILD_DIR=$PROJECT_DIR/triad-puzzle.Frontend/build
+WEB_DIR=$PROJECT_DIR/triad-puzzle.Web
+
+cp $BUILD_DIR/assets/*.* $WEB_DIR/assets
+cp $BUILD_DIR/assets/fonts/*.* $WEB_DIR/assets/fonts
+cp $BUILD_DIR/icons/*.* $WEB_DIR
+cp $BUILD_DIR/index.html $WEB_DIR
